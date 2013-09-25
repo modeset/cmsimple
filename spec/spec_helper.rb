1ef64df71bc6ee22f2f475a1a71a06de9a56ad77
Jed Schneider <jed.schneider@modeset.com>
2013-09-25 16:38:32 -0400

    use database cleaner for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'shoulda/matchers'
 require 'timecop'
 require 'capybara'
 require 'capybara/rspec'
+require 'database_cleaner'
 require 'selenium-webdriver'
 require 'site_prism'
 
@@ -29,7 +30,22 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
-end
+  config.use_transactional_fixtures = false
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each, js: true) do
+    DatabaseCleaner.clean
+  end
+end
 
